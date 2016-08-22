089339e9fa8087d6435d317f8e241d8d528484d3
Daniela Noir <daniela@danielanoir.com>
2016-08-22 15:50:27 -0400

    updates

--- js/categories/category.factory.js
+++ js/categories/category.factory.js
@@ -9,7 +9,7 @@
     ]);
 
     function CategoryFactoryFunction($resource){
-      return $resource("http://localhost:3000/categories/:id", {}, {
+      return $resource("https://dapi-app.herokuapp.com/categories/:id", {}, {
         update: { method: "PUT" }
       });
     }
